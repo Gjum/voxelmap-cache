9f789ea9b3cea7ef43cc0db38277501c7d012f70
Gjum <code.gjum@gmail.com>
2017-04-08 20:56:05 +0200

    keep timestamps when re-zipping regions

--- py/rezip_cache.py
+++ py/rezip_cache.py
@@ -1,10 +1,12 @@
 """
-Sometimes, Rust's zip reader can't open some region cache .zip files.
-This script re-zips every region in a way that Rust can read them.
+Sometimes Rust's zip reader can't open some region cache .zip files.
+This script re-zips every region in a way that Rust can read them,
+keeping their timestamps intact.
 """
 import os
 import sys
 import time
+from shutil import copystat, move
 from zipfile import ZipFile
 
 def main():
@@ -19,26 +21,31 @@ def main():
 def rezip_all(target_dir, source_dir):
     os.makedirs(target_dir, exist_ok=True)
 
-    regions = [tuple(map(int, region[:-4].split(',')))
-             for region in os.listdir(source_dir)
-             if region[-4:] == '.zip']
+    regions = [region for region in os.listdir(source_dir) if region[-4:] == '.zip']
 
     print('re-zipping', len(regions), 'regions ...')
 
     first_progress = last_progress = time.time()
-    for rn, pos in enumerate(regions):
+    for rn, region in enumerate(regions):
         if last_progress + 3 < time.time():
             last_progress += 3
             time_left = (time.time() - first_progress) / rn * (len(regions) - rn)
             print('re-zip: %i/%i regions' % (rn, len(regions)),
                 '%i:%02i left' % (int(time_left / 60), int(time_left % 60)))
 
-        with ZipFile(source_dir + '/%s,%s.zip' % pos).open('data') as f:
+        source_zip = source_dir + '/' + region
+        target_zip = target_dir + '/' + region
+
+        with ZipFile(source_zip).open('data') as f:
             data = f.read()
 
-        zf = ZipFile(target_dir + '/%s,%s.zip' % pos, 'w')
+        # write to intermediate file in case source_dir == target_dir
+        zf = ZipFile(target_zip+'.new', 'w')
         zf.writestr('data', data)
         zf.close()
+
+        copystat(source_zip, target_zip+'.new')
+        move(target_zip+'.new', target_zip)
 
     print('Done, new cache is at', target_dir)
 
